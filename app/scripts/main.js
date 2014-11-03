f82ac852f02316ab922ebf2d8ca60409c2984964
gurimmer <s.reianai@gmail.com>
2014-11-04 00:18:45 +0900

    ライブビデオ部分作成

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,413 @@
 
+var ctx, color = "black";
+var socket, localStream, peerConnection = undefined;
+var CR = String.fromCharCode(13);
+
+var localVideo = document.getElementById('local-video');
+var remoteVideo = document.getElementById('remote-video');
+
+function newCanvas(){
+    var canvas = '<canvas id="canvas" width="345px" height="250px"></canvas>';
+    $("#drawCanvas").html(canvas);
+    ctx = document.getElementById("canvas").getContext("2d");
+    ctx.strokeStyle = color;
+    ctx.lineWidth = 3;
+    $("#canvas").drawTouch();
+    $("#canvas").drawPointer();
+    $("#canvas").drawMouse();
+}
+
+$.fn.drawTouch = function() {
+    var start = function(e) {
+        e = e.originalEvent;
+        ctx.beginPath();
+        x = e.changedTouches[0].pageX;
+        y = e.changedTouches[0].pageY;
+        ctx.moveTo(x,y);
+    };
+    var move = function(e) {
+        e.preventDefault();
+        e = e.originalEvent;
+        x = e.changedTouches[0].pageX;
+        y = e.changedTouches[0].pageY;
+        ctx.lineTo(x,y);
+        ctx.stroke();
+    };
+    $(this).on("touchstart", start);
+    $(this).on("touchmove", move);
+};
+
+$.fn.drawPointer = function() {
+    var start = function(e) {
+        e = e.originalEvent;
+        ctx.beginPath();
+        x = e.pageX;
+        y = e.pageY;
+        ctx.moveTo(x,y);
+    };
+    var move = function(e) {
+        e.preventDefault();
+        e = e.originalEvent;
+        x = e.pageX;
+        y = e.pageY;
+        ctx.lineTo(x,y);
+        ctx.stroke();
+    };
+    $(this).on("MSPointerDown", start);
+    $(this).on("MSPointerMove", move);
+};
+
+$.fn.drawMouse = function() {
+    var clicked = 0;
+    var start = function(e) {
+        clicked = 1;
+        ctx.beginPath();
+        x = e.offsetX;
+        y = e.offsetY;
+        ctx.moveTo(x, y);
+        socket.emit('message',{
+            act: 'down',
+            x: x,
+            y: y,
+            color: ctx.strokeStyle
+        });
+    };
+    var move = function(e) {
+        if(clicked){
+            x = e.offsetX;
+            y = e.offsetY;
+            ctx.lineTo(x, y);
+            ctx.stroke();
+            socket.emit('message',{
+                act: 'move',
+                x: x,
+                y: y
+            });
+        }
+    };
+    var stop = function(e) {
+        clicked = 0;
+    };
+    $(this).on("mousedown", start);
+    $(this).on("mousemove", move);
+    $(window).on("mouseup", stop);
+};
+
 $(function(){
-    console.log('\'Allo \'Allo!');
+
+    var socketView = new Vue({
+        el: '.live-video-box',
+        template: '#live-video-tmpl',
+        data: {
+            socketReady: false,
+            port: 9001,
+            sendSdp: '',
+            sendIce: '',
+            receiveSdp: '',
+            receiveIce: '',
+            iceSeparator: '------ ICE Candidate -------',
+            peerStarted: false,
+            remote_down: false,
+            mediaConstraints: {
+                'mandatory': {
+                    'OfferToReceiveAudio':false,
+                    'OfferToReceiveVideo':true
+                }
+            }
+        },
+        ready: function() {
+            this.init();
+        },
+        methods: {
+            onOpend: function(e) {
+                console.log('socket opened');
+                socketView.socketReady = true;
+                socketView.startVideo();
+            },
+            onMessage: function(evt) {
+                console.log('socket message');
+                if (evt.type === 'offer') {
+                    console.log("Received offer, set offer, sending answer....");
+                    socketView.onOffer(evt);
+                } else if (evt.type === 'answer' && socketView.peerStarted) {
+                    console.log('Received answer, settinng answer SDP');
+                    socketView.onAnswer(evt);
+                } else if (evt.type === 'candidate' && socketView.peerStarted) {
+                    console.log('Received ICE candidate...');
+                    this.onCandidate(evt);
+                } else if (evt.type === 'user dissconnected' && socketView.peerStarted) {
+                    console.log("disconnected");
+                    socketView.stop();
+                } else {
+                    switch (evt.act) {
+                        case "down":
+                            socketView.remote_down = true;
+                            ctx.strokeStyle = evt.color;
+                            ctx.beginPath();
+                            ctx.moveTo(evt.x, evt.y);
+                        case "move":
+                            console.log("remote: " + evt.x, evt.y);
+                            ctx.lineTo(evt.x, evt.y);
+                            ctx.stroke();
+                        case "up":
+                            if (!socketView.remote_down) return;
+                            ctx.lineTo(evt.x, evt.y);
+                            ctx.stroke();
+                            ctx.closePath();
+                            socketView.remote_down = false;
+                    }
+                }
+            },
+            init: function() {
+                socket = io.connect('http://localhost:' + this.port + '/');
+                socket.on('connect', this.onOpend)
+                      .on('message', this.onMessage);
+
+            },
+
+
+
+
+            onSDP: function(evt) {
+                var evt = JSON.parse(this.receiveSdp);
+                if (peerConnection) {
+                    this.onAnswer(evt);
+                }
+                else {
+                    this.onOffer(evt);
+                }
+                this.receiveSdp = "";
+            },
+
+            onICE: function() {
+                var text = this.receiveIce;
+                var arr = text.split(iceSeparator);
+                for (var i = 1, len = arr.length; i < len; i++) {
+                    var evt = JSON.parse(arr[i]);
+                    this.onCandidate(evt);
+                }
+                this.receiveIce = "";
+            },
+
+            onOffer: function(evt) {
+                console.log("Received offer...");
+                console.log(evt);
+                this.setOffer(evt);
+                this.sendAnswer(evt);
+                this.peerStarted = true;
+            },
+
+            onAnswer: function(evt) {
+                console.log("Received Answer...");
+                console.log(evt);
+                this.setAnswer(evt);
+            },
+
+            onCandidate: function(evt) {
+                var candidate = new RTCIceCandidate({sdpMLineIndex:evt.sdpMLineIndex, sdpMid:evt.sdpMid, candidate:evt.candidate});
+                console.log("Received Candidate...");
+                console.log(candidate);
+                peerConnection.addIceCandidate(candidate);
+            },
+
+            sendSDP: function(sdp) {
+                var text = JSON.stringify(sdp);
+                console.log("---sending sdp text ---");
+                console.log(text);
+                this.sendSdp = text;
+
+                // send via socket
+                socket.json.send(sdp);
+            },
+
+            sendCandidate: function(candidate) {
+                var text = JSON.stringify(candidate);
+                console.log("---sending candidate text ---");
+                console.log(text);
+                this.sendIce = (this.sendIce + CR + iceSeparator + CR + text + CR);
+                //this.sendIce.scrollTop = textForSendICE.scrollHeight;
+
+                // send via socket
+                socket.json.send(candidate);
+            },
+
+            startVideo: function() {
+                navigator.webkitGetUserMedia({video: true, audio: true},
+                    function (stream) { // success
+                        localStream = stream;
+                        localVideo.src = window.webkitURL.createObjectURL(stream);
+                        localVideo.play();
+                        localVideo.volume = 0;
+                    },
+                    function (error) { // error
+                        console.error('An error occurred: [CODE ' + error.code + ']');
+                        return;
+                    }
+                );
+            },
+
+            stopVideo: function() {
+                localVideo.src = "";
+                localStream.stop();
+            },
+
+            prepareNewConnection: function() {
+                var pc_config = {"iceServers":[]};
+                var peer = null;
+                try {
+                    peer = new webkitRTCPeerConnection(pc_config);
+                } catch (e) {
+                    console.log("Failed to create peerConnection, exception: " + e.message);
+                }
+
+                peer.onicecandidate = function (evt) {
+                    if (evt.candidate) {
+                        console.log(evt.candidate);
+                        this.sendCandidate({type: "candidate",
+                                sdpMLineIndex: evt.candidate.sdpMLineIndex,
+                                sdpMid: evt.candidate.sdpMid,
+                                candidate: evt.candidate.candidate}
+                        );
+                    } else {
+                        console.log("End of candidates. ------------------- phase=" + evt.eventPhase);
+                    }
+                };
+
+                console.log('Adding local stream...');
+                peer.addStream(localStream);
+
+                peer.addEventListener("addstream", this.onRemoteStreamAdded, false);
+                peer.addEventListener("removestream", this.onRemoteStreamRemoved, false)
+
+                return peer;
+            },
+
+            onRemoteStreamAdded: function(event) {
+                console.log("Added remote stream");
+                remoteVideo.src = window.webkitURL.createObjectURL(event.stream);
+            },
+
+            // when remote removes a stream, remove it from the local video element
+            onRemoteStreamRemoved: function(event) {
+                console.log("Remove remote stream");
+                remoteVideo.src = "";
+            },
+
+            sendOffer: function() {
+                peerConnection = prepareNewConnection();
+                peerConnection.createOffer(function (sessionDescription) { // in case of success
+                    peerConnection.setLocalDescription(sessionDescription);
+                    console.log("Sending: SDP");
+                    console.log(sessionDescription);
+                    this.sendSDP(sessionDescription);
+                }, function () { // in case of error
+                    console.log("Create Offer failed");
+                }, this.mediaConstraints);
+            },
+
+            setOffer: function(evt) {
+                if (peerConnection) {
+                    console.error('peerConnection alreay exist!');
+                }
+                peerConnection = prepareNewConnection();
+                peerConnection.setRemoteDescription(new RTCSessionDescription(evt));
+            },
+
+            sendAnswer: function(evt) {
+                console.log('sending Answer. Creating remote session description...' );
+                if (! peerConnection) {
+                    console.error('peerConnection NOT exist!');
+                    return;
+                }
+
+                peerConnection.createAnswer(function (sessionDescription) { // in case of success
+                    peerConnection.setLocalDescription(sessionDescription);
+                    console.log("Sending: SDP");
+                    console.log(sessionDescription);
+                    this.sendSDP(sessionDescription);
+                }, function () { // in case of error
+                    console.log("Create Answer failed");
+                }, this.mediaConstraints);
+            },
+
+            setAnswer: function(evt) {
+                if (! peerConnection) {
+                    console.error('peerConnection NOT exist!');
+                    return;
+                }
+                peerConnection.setRemoteDescription(new RTCSessionDescription(evt));
+            },
+
+            connect: function() {
+                if (!this.peerStarted && localStream && this.socketReady) { // **
+                    //if (!peerStarted && localStream) { // --
+                    this.sendOffer();
+                    this.peerStarted = true;
+                } else {
+                    alert("Local stream not running yet - try again.");
+                }
+            },
+
+            // stop the connection upon user request
+            hangUp: function() {
+                console.log("Hang up.");
+                this.stop();
+            },
+
+            stop: function() {
+                peerConnection.close();
+                peerConnection = null;
+                this.peerStarted = false;
+            }
+
+        }
+    });
+
+    var toolbar = new Vue({
+        el: ".toolbar",
+        data: {
+            palettes: [
+                {color: 'black', active: true},
+                {color: 'red', active: false},
+                {color: 'green', active: false},
+                {color: 'blue', active: false}
+            ],
+            brushs: [
+                {sizeName: 'S', width: 3, active: true},
+                {sizeName: 'M', width: 6, active: false},
+                {sizeName: 'L', width: 9, active: false}
+            ]
+        },
+        ready: function() {
+            newCanvas();
+            this.setColor(this.palettes[0]);
+            this.setBrush(this.brushs[0]);
+        },
+        methods: {
+            setColor: function(palette) {
+                this.clearActivePalette();
+                palette.active = true;
+                ctx.beginPath();
+                ctx.strokeStyle = palette.color;
+            },
+            setBrush: function(brush) {
+                this.clearActiveBrush();
+                brush.active = true;
+                ctx.beginPath();
+                ctx.lineWidth = brush.width;
+            },
+            clearActivePalette: function() {
+                $.each(this.palettes, function(palette){
+                    palette.active = false;
+                });
+            },
+            clearActiveBrush: function() {
+                $.each(this.brushs, function(brush){
+                    brush.active = false;
+                });
+            }
+        }
+    });
 
     var chat = new Vue({
         el: ".chat",
@@ -34,46 +441,6 @@ $(function(){
         }
     });
 
-    var liveCanvas = new Vue({
-        el: "#drawCanvas",
-        data: {
-            ctx: undefined,
-            x: 0,
-            y: 0,
-            isStart: false
-        },
-        template: '#liveCanvas',
-        created: function() {
 
-        },
-        ready: function() {
-            //this.ctx = document.getElementById("canvas").getContext("2d");
-            //this.ctx.strokeStyle = '#000';
-            //this.ctx.lineWidth = 5;
-        },
-        methods: {
-            start: function(e) {
-                console.log('start');
-                this.isStart = true;
-                this.ctx.beginPath();
-                this.x = e.pageX;
-                this.y = e.pageY;
-                this.ctx.moveTo(this.x, this.y);
-            },
-            end: function(e) {
-                console.log('end');
-                this.isStart = false;
-            },
-            move: function(e) {
-                if (this.isStart) {
-                    console.log('move');
-                    this.x = e.pageX;
-                    this.y = e.pageY;
-                    this.ctx.lineTo(this.x, this.y);
-                    this.ctx.stroke();
-                }
-            }
-        }
-    });
 
 });
